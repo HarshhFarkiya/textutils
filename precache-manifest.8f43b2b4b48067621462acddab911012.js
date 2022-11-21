de29d2f8805cde5715fb05f8e8823f7b3289df74
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2022-11-22 00:57:54 +0530

    Updates

---
+++ precache-manifest.8f43b2b4b48067621462acddab911012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11ed27cb5ef1628bc3bf",
+    "url": "/textutils/static/css/main.b31a0f80.chunk.css"
+  },
+  {
+    "revision": "11ed27cb5ef1628bc3bf",
+    "url": "/textutils/static/js/main.11ed27cb.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/textutils/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "0fe314140e0aadaefa3b",
+    "url": "/textutils/static/js/2.0fe31414.chunk.js"
+  },
+  {
+    "revision": "304862408bccdfaa3b11",
+    "url": "/textutils/static/js/runtime~main.30486240.js"
+  },
+  {
+    "revision": "4b03d6edabb04ccdf8ef61f615e43100",
+    "url": "/textutils/index.html"
+  }
+];
